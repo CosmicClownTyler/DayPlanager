3e3d683ebe13b4134d275c63cd7f18127912e227
Tyler <TylerADalke22@hotmail.com>
2026-01-14 21:36:55 -0600

    Added Page component.

---
+++ src/components/layout/Page/Page.tsx
@@ -0,0 +1,19 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { useThemeColors } from '@/src/hooks/settings/theme/';
+
+import { pageStyles } from './styles';
+import type { PageProps } from './types';
+
+export function Page(props: PageProps) {
+    // Deconstruct props
+    const { children } = props;
+
+    const themeColors = useThemeColors();
+
+    return (
+        <SafeAreaView style={[pageStyles.container, { backgroundColor: themeColors.background }]}>
+            {children}
+        </SafeAreaView>
+    );
+};
---
+++ src/components/layout/Page/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export const pageStyles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+});
